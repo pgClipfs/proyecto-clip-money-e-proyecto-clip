759e1ca96d259b6f0a634d04dfb6aa3640332cdd
ArmatorX <17768182+ArmatorX@users.noreply.github.com>
2021-01-06 10:57:10 -0300

    creación del backend

---
+++ backend/src/main/java/ProgramaClip/ClipMoneyAPI/ClipMoneyApiApplication.java
@@ -0,0 +1,13 @@
+package ProgramaClip.ClipMoneyAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClipMoneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClipMoneyApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ProgramaClip/ClipMoneyAPI/ClipMoneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ProgramaClip.ClipMoneyAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClipMoneyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
