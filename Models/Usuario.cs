8cb5b4d4ef2293a71d700e23fd0bc0731012aee5
ArmatorX <17768182+ArmatorX@users.noreply.github.com>
2021-01-20 11:02:49 -0300

    Constructor y propiedades de la clase Usuario.

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -13,5 +13,22 @@ namespace proyecto_clip_money_e_proyecto_clip.Models
         private DatosUsuario datosUsuario;
         private List<Factura> facturas;
         private List<Cuenta> cuentas;
+
+        public Usuario(long id, string nick, string contrasena, DatosUsuario datosUsuario, List<Factura> facturas, List<Cuenta> cuentas)
+        {
+            this.Id = id;
+            this.Nick = nick;
+            this.Contrasena = contrasena;
+            this.DatosUsuario = datosUsuario;
+            this.Facturas = facturas;
+            this.Cuentas = cuentas;
+        }
+
+        public long Id { get => id; set => id = value; }
+        public string Nick { get => nick; set => nick = value; }
+        public string Contrasena { get => contrasena; set => contrasena = value; }
+        public DatosUsuario DatosUsuario { get => datosUsuario; set => datosUsuario = value; }
+        public List<Factura> Facturas { get => facturas; set => facturas = value; }
+        public List<Cuenta> Cuentas { get => cuentas; set => cuentas = value; }
     }
 }
